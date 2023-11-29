8ad6c0e1f0b8347c5ecd2784712404f099f9c522
Andy Wojnarek <andy.wojnarek@theatsgroup.com>
2023-11-29 12:27:13 -0500

    WIP

--- tf2.js
+++ tf2.js
@@ -17,19 +17,49 @@ const CHANNEL_ID = config.CHANNEL_ID;
 const TF2_SERVER_IP = config.TF2_SERVER_IP;
 const TF2_SERVER_PORT = config.TF2_SERVER_PORT
 
+
+
+//////////
+// Main //
+//////////
 let messageToUpdate = null;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-
 client.once('ready', () => {
     console.log('Bot is online!');
     initializeMessage();
 });
 
+
+///////////////
+// Functions //
+///////////////
+function updateMessage(isOnline, playerCount = 0, maxPlayers = 0) {
+    const embed = new MessageEmbed()
+        .setTitle('Server Status')
+        .setColor(isOnline ? '#00FF00' : '#FF0000') // Green if online, red if offline
+        .setDescription(isOnline ? `:green_square: Online (${playerCount}/${maxPlayers})` : ':red_square: Offline');
+
+    const joinButton = new MessageButton()
+        .setStyle('LINK')
+        .setLabel('Join Server')
+        .setURL(`steam://connect/${TF2_SERVER_IP}:${TF2_SERVER_PORT}`)
+        .setDisabled(!isOnline);
+
+    const row = new MessageActionRow().addComponents(joinButton);
+
+    if (messageToUpdate) {
+        messageToUpdate.edit({ embeds: [embed], components: [row] });
+    }
+}
+
 function initializeMessage() {
     const channel = client.channels.cache.get(CHANNEL_ID);
     if (channel) {
-        channel.send('Initializing server status...').then(msg => {
+        const embed = new MessageEmbed()
+            .setTitle('Server Status')
+            .setDescription('Checking server status...');
+        channel.send({ embeds: [embed] }).then(msg => {
             messageToUpdate = msg;
             queryTF2ServerPeriodically();
         });
@@ -44,11 +74,10 @@ function queryTF2Server() {
         host: TF2_SERVER_IP,
         port: TF2_SERVER_PORT
     }).then((state) => {
-        const playerCount = state.players.length;
-        const newMessage = `There are currently ${playerCount} players on The Gravity Bong.`;
-        updateMessage(newMessage);
+        updateMessage(true, state.players.length, state.maxplayers);
     }).catch((error) => {
         console.error("Server is offline or IP is incorrect", error);
+        updateMessage(false);
     });
 }
 
